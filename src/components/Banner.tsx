3687d674a2fdbf698130ada98ff715dadbbda66c
Ratakar777 <w0062870@gmail.com>
2025-06-10 15:57:25 +0300

    anim

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -13,6 +13,7 @@ interface SectionProps {
   title: string;
   icon?: string;
   index: number;
+  href?: string;
 }
 
 const FullSection = styled(Box)({
@@ -22,36 +23,42 @@ const FullSection = styled(Box)({
   alignItems: "center",
   backgroundColor: "#ffffff",
   justifyContent: "start",
+  overflow: "hidden",
 });
 
-const ContentWrapper = styled(Box)<{ inview: number }>(({ inview }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  gap: "20px",
-  padding: "20px",
-  cursor: "pointer",
-  transition: "all 0.8s cubic-bezier(0.4, 0, 0.2, 1)",
-  transform: inview
-    ? "translate(0, 0)"
-    : `translate(${(Math.random() - 0.5) * 100}%, ${
-        (Math.random() - 0.5) * 100
-      }%)`,
-  opacity: inview ? 1 : 0,
-  "&:hover": {
-    "& img": {
-      transform: "scale(1.1)",
+const ContentWrapper = styled("a")<{ inview: number; index: number }>(
+  ({ inview, index }) => ({
+    textDecoration: "none",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+    padding: "20px",
+    cursor: "pointer",
+    transition: "all 0.8s cubic-bezier(0.4, 0, 0.2, 1)",
+    transform: inview
+      ? "translate(0, 0)"
+      : index === 5
+      ? `translate(0, 200%)`
+      : `translate(${index % 2 === 0 ? "-50%" : "50%"}, 50%) `,
+    opacity: inview ? 1 : 0,
+    willChange: "transform, opacity",
+    "&:hover": {
+      "& img": {
+        transform: "scale(1.1)",
+      },
+      "& h4": {
+        color: "#000000",
+      },
     },
-    "& h4": {
-      color: "#000000",
-    },
-  },
-}));
+  })
+);
 
-const Section: React.FC<SectionProps> = ({ title, icon, index }) => {
+const Section: React.FC<SectionProps> = ({ title, icon, index, href }) => {
   const { ref, inView } = useInView({
-    threshold: 0.5,
+    threshold: 0.2,
     triggerOnce: false,
+    rootMargin: "50px",
   });
 
   const isMobile = useMediaQuery((theme: Theme) =>
@@ -66,7 +73,33 @@ const Section: React.FC<SectionProps> = ({ title, icon, index }) => {
             index >= 4 ? "center" : index % 2 === 0 ? "start" : "end",
         }}
       >
-        <ContentWrapper ref={ref} inview={inView ? 1 : 0}>
+        <ContentWrapper
+          ref={ref}
+          inview={inView ? 1 : 0}
+          href={href}
+          target='_blank'
+          index={index}
+          onClick={(e) => {
+            if (index === 5) {
+              window.navigator.clipboard.writeText(
+                "7Yn7m5noMaMq8jLjRXNBLw8EEw7WPehJRJZm6szRpump"
+              );
+              const target = e.currentTarget;
+              target.style.transform = "scale(0.6)";
+              target.style.transition = "transform 0.3s ease-in";
+
+              setTimeout(() => {
+                target.style.transform = "scale(1.25)";
+                target.style.transition = "transform 0.3s ease-out";
+
+                setTimeout(() => {
+                  target.style.transform = "scale(1)";
+                  target.style.transition = "transform 0.3s ease-out";
+                }, 300);
+              }, 300);
+            }
+          }}
+        >
           <Box
             display='flex'
             alignItems='center'
@@ -88,9 +121,12 @@ const Section: React.FC<SectionProps> = ({ title, icon, index }) => {
             <Typography
               variant='h1'
               sx={{
+                fontWeight: 900,
+                "-webkit-text-stroke": "3px #473732",
                 color: "#473732",
                 transition: "color 0.3s ease-in-out",
-                fontSize: isMobile ? "2rem" : "3rem",
+                fontSize: isMobile ? "1.8rem" : index === 5 ? "2rem" : "3rem",
+                wordBreak: "break-word",
               }}
             >
               {title}
@@ -103,12 +139,24 @@ const Section: React.FC<SectionProps> = ({ title, icon, index }) => {
 };
 
 const sections = [
-  { title: "Dexscreener", icon: "/3.png" },
-  { title: "Dextools", icon: "/1.png" },
-  { title: "Telegram", icon: "/2.png" },
-  { title: "Twitter", icon: "/5.png" },
-  { title: "Buy Now", icon: "/4.png" },
-  { title: "CA: Contract" },
+  {
+    title: "Dexscreener",
+    icon: "/3.png",
+    href: "https://dexscreener.com/solana/7Yn7m5noMaMq8jLjRXNBLw8EEw7WPehJRJZm6szRpump",
+  },
+  {
+    title: "Dextools",
+    icon: "/1.png",
+    href: "https://www.dextools.io/app/en/solana/pair-explorer/3mTiHk798Z1BGBJKw95Qr99LG6J24JMagxRcSFRMrbfN?t=1750152508976",
+  },
+  { title: "Telegram", icon: "/2.png", href: "https://t.me/nig_cto" },
+  { title: "Twitter", icon: "/5.png", href: "https://x.com/nig_cto" },
+  {
+    title: "Buy Now",
+    icon: "/4.png",
+    href: "https://swap.pump.fun/?input=So11111111111111111111111111111111111111112&output=7Yn7m5noMaMq8jLjRXNBLw8EEw7WPehJRJZm6szRpump",
+  },
+  { title: "CA: 7Yn7m5noMaMq8jLjRXNBLw8EEw7WPehJRJZm6szRpump" },
 ];
 
 const Banner: React.FC = () => {
@@ -139,6 +187,7 @@ const Banner: React.FC = () => {
           title={section.title}
           icon={section.icon}
           index={index}
+          href={section.href}
         />
       ))}
     </Box>
